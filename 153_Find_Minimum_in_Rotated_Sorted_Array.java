c4dc45ac926ee8f993167a69234301edffc6518e
昆仑 <kunlun@taobao.com>
2018-05-09 11:19:38 +0800

    java solution of 153

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,19 @@
+public static int findMin(int[] a) {
+    if (a == null) return -1;
+    int l = 0;
+    int r = a.length-1;
+    int n = a.length;
+    r = n - 1
+	while (l < r) {
+	    int m = l + (r-l)/2;
+	    if (a[m] < a[n-1]) { // left a[m] < a[r]
+		if (m-1 >= 0 && a[m] < a[m-1]) {
+		    return a[m];
+		} 
+		r = m; 
+	    } else {    // right
+		l = m;
+	    }
+	}
+    return a[l];
+}
